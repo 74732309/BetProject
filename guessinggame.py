07086a80e57e5cb4af51f226208088fbb3eb0ce2
SarahdaHora <97102774+74732309@users.noreply.github.com>
2024-01-23 16:09:10 -0300

    Update guessinggame.py

--- guessinggame.py
+++ guessinggame.py
@@ -1,15 +1,15 @@
 import random
-print('I'm your computer...')
-print('I just thought of a number between 0 and 10.')
-print('Can you guess which one it was?')
-x = int(input('What is your guess: '))
+print("I'm your computer...")
+print("I just thought of a number between 0 and 10.")
+print("Can you guess which one it was?")
+x = int(input("What is your guess: "))
 y = random.randint(0,10)
 while x != y:
      if y > x:
-        print('More... Try again...')
-        x = int(input('What's your guess? '))
+        print("More... Try again...")
+        x = int(input("What's your guess? "))
      else:
-        print('Less... Try again...')
-        x = int(input('What's your guess? '))
+        print("Less... Try again...")
+        x = int(input("What's your guess? "))
 print("Congratulations, you got it right!!!")
     
